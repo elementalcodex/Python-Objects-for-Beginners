811a0c8cc73307cb7a52323ebeed07b9a9032ed3
elementalcodex <133904570+elementalcodex@users.noreply.github.com>
2023-05-25 15:57:35 -0400

    Update dog.py

--- dog.py
+++ dog.py
@@ -3,7 +3,7 @@ import time
 from IPython.display import Audio, display
 
 class Dog:
-  def bark(self, sound):
+  def bark(self, sound = 'barks.wav'):
     display(Audio(sound, autoplay=True))
     time.sleep(2)
 print('woof')
