e46d52a11d5420f1177b236b87bde2dd8e9af414
elementalcodex <133904570+elementalcodex@users.noreply.github.com>
2023-05-26 14:56:33 -0400

    Update dog.py

--- dog.py
+++ dog.py
@@ -4,10 +4,10 @@ from IPython.display import Audio, display
 
 class Dog:
   def __init__(self):
-    d = '/content/Python-Objects-for-Beginners/Dog/'
-    self.bark = d + 'bark.wav'
-    self.sniff = d + 'sniff.wav'
-    self.whimper = d + 'whimper.wav'
+    d = '/content/Python-Objects-for-Beginners/'
+    self.bark = d + 'barks.wav'
+    self.sniff = d + 'sniffs.wav'
+    self.whimper = d + 'whimpers.wav'
     
   def sound(self, sound_name):
     display(Audio(sound_name, autoplay=True))
