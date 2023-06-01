0ab95948bb4e00c60d7bd8e3b900473cb7d62190
elementalcodex <133904570+elementalcodex@users.noreply.github.com>
2023-06-01 11:46:37 -0400

    Update dog.py

--- dog.py
+++ dog.py
@@ -35,4 +35,11 @@ class Dog:
     self.__image__('swim' + str(rint(1,4)) + '.png')
   def sleep(self):
     self.__image__('sleep' + str(rint(1,4)) + '.png')
-
+  def __add__(self, add_num):
+    self.health  += add_nun
+  def __add__(self, add_num):
+    self.health  = min(self.health + rint(0, add_num), 100)
+    self.health  = min(self.health + rint(0, add_num), 100)
+    self.health  = min(self.health + rint(0, add_nun), 100)
+   
+    
